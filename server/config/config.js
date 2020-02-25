93c508d892df6554ab9ac30ca7ee94e8fe03a0f0
Rafael <guzmanrafael0456@gmail.com>
2020-02-24 20:12:08 -0700

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://rafael:rafael123@cafe-pkoom.mongodb.net/test?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
