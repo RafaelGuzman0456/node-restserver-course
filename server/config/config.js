a51982419df4bef34360d77103452dcfde90ce9f
Rafael <guzmanrafael0456@gmail.com>
2020-02-24 19:46:43 -0700

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 // Puerto
 //============
 process.env.PORT = process.env.PORT || 3000;
+
+//============
+// Entorno
+//============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//============
+// DATABASES
+//============
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://rafael:rafael123@cafe-pkoom.mongodb.net/test?retryWrites=true&w=majority';
+}
+process.env.URLDB = urlDB;
---
+++ server/models/user.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol valido'
+};
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    name: {
+        type: String,
+        unique: false,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El correo es necesario']
+    },
+    password: {
+        type: String,
+        require: [true, 'La contraseña es obligatoria']
+    },
+    img: {
+        type: String,
+        require: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    state: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+userSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
+
+userSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
+
+module.exports = mongoose.model('User', userSchema);
